d2ce43a8299ec5d956d457dc0ed8fe638b54ed7a
badr <technobadr2003@gmail.com>
2025-02-17 16:45:47 +0100

    modifying cassandra's schema for better visualization on grafana, with minor changes to the spark-stream service to wait until cassandra starts

--- src/cassandra/cassandra_manager.py
+++ src/cassandra/cassandra_manager.py
@@ -2,6 +2,7 @@ import os, sys
 from cassandra.cluster import Cluster
 import uuid
 from cassandra.auth import PlainTextAuthProvider
+from datetime import datetime
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.cassandra.cassandra_queries import CassandraQueries
@@ -18,14 +19,14 @@ class CassandraManager:
         auth_provider = PlainTextAuthProvider("admin", "admin")
         self.cluster = Cluster([self.host], auth_provider=auth_provider)
         self.session = self.cluster.connect()
-
+                
         self.session.execute(CassandraQueries.CREATE_KEYSPACE.format(keyspace=self.keyspace))
         self.session.set_keyspace(self.keyspace)
 
         self.session.execute(CassandraQueries.CREATE_TABLE)
 
     def insert_prediction(self, text: str, prediction: str, subreddit: str):
-        self.session.execute(CassandraQueries.INSERT_PREDICTION, (uuid.uuid4(), text, prediction, subreddit))
+        self.session.execute(CassandraQueries.INSERT_PREDICTION, (uuid.uuid4(), text, prediction, subreddit, datetime.now()))
 
     def close(self):
         if self.cluster:
--- src/cassandra/cassandra_queries.py
+++ src/cassandra/cassandra_queries.py
@@ -6,11 +6,13 @@ class CassandraQueries:
 
     CREATE_TABLE = """
     CREATE TABLE IF NOT EXISTS prediction_results (
-        id UUID PRIMARY KEY,
+        id UUID,
         text TEXT,
         prediction TEXT,
-        subreddit TEXT
+        subreddit TEXT,
+        timestamp TIMESTAMP,
+        PRIMARY KEY ((prediction), timestamp, id)
     );
     """
 
-    INSERT_PREDICTION = "INSERT INTO prediction_results (id, text, prediction, subreddit) VALUES (%s, %s, %s, %s);"
+    INSERT_PREDICTION = "INSERT INTO prediction_results (id, text, prediction, subreddit, timestamp) VALUES (%s, %s, %s, %s, %s);"
